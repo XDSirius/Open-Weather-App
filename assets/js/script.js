83e89d0f38ee2b6058dad552f7263a4635bba6df
Yasmin Castillo <yasmin_c@outlook.com>
2023-06-26 18:05:09 -0500

    api key is linked

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ console.log("hi");
 
 const apiKey = '0ed334a1b75961ee558dfd6ad924e663';
 
-const apiUrl = 'api.openweathermap.org/data/2.5/weather';
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=cincinnati&appid='+ apiKey;
 //https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 function checkWeather(){
         fetch(apiUrl)
